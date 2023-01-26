5bf59e799c700f79e3666c840989e9983de3929d
Samuel Kalumba <kalumbasml@gmail.com>
2023-01-26 13:38:26 +0000

    contact link correction

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
 urlpatterns = [
 	path('', views.IndexView.as_view(), name="main"),
 	path('about/', views.AboutView.as_view(), name="about"),
+	path('travel/', views.TravelView.as_view(), name="travel"),
+	path('article/', views.ArticleView.as_view(), name="article"),
+	path('privacy-policy/', views.PrivacyPolicyView.as_view(), name="privacy-policy"),
+	path('terms-conditions/', views.TermsConditionsView.as_view(), name="terms-conditions"),
+    path('404/', views.PageNotFoundView.as_view(), name="404"),
+    path('contact/', views.ContactView.as_view(), name="contact"),
 ]
--- main/views.py
+++ main/views.py
@@ -7,4 +7,22 @@ class IndexView(View):
 		return render(request, self.template_file, {})
 
 class AboutView(IndexView):
-    template_file = 'main/about.html'
+    template_file = 'main/article.html'
+
+class ContactView(IndexView):
+    template_file = 'main/contact.html'
+
+class TravelView(IndexView):
+    template_file = 'main/travel.html'
+
+class ArticleView(IndexView):
+    template_file = 'main/article.html'
+
+class PrivacyPolicyView(IndexView):
+    template_file = 'main/privacy-policy.html'
+
+class TermsConditionsView(IndexView):
+    template_file = 'main/terms-conditions.html'
+
+class PageNotFoundView(IndexView):
+    template_file = 'main/404.html'
